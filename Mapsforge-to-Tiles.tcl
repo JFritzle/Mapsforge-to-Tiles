8ede50f3ba5350f669cc292d4d384b8030422155
JFritzle <62614244+JFritzle@users.noreply.github.com>
2023-09-05 10:15:27 +0200

    Update files via upload

--- Mapsforge-to-Tiles.tcl
+++ Mapsforge-to-Tiles.tcl
@@ -550,12 +550,13 @@ set maps [lsort -dictionary $maps]
 if {[llength $maps] == 0} {error_message [mc e11] exit}
 
 # Get list of available Mapsforge themes
-# and add Mapsforge built-in default theme
+# and add Mapsforge built-in themes
 
 cd $themes_folder
 set themes [find_files "" "*.xml"]
 cd $cwd
-lappend themes "(default)"
+if {$::server_version <  2000} {lappend themes "(default)"}
+if {$::server_version >= 2000} {lappend themes "(DEFAULT)" "(OSMARENDER)"}
 set themes [lsort -dictionary $themes]
 
 # --- Begin of main window left column
@@ -1581,10 +1582,11 @@ proc setup_styles_overlays_structure {} {
     foreach child [winfo children .overlays] {destroy $child}
   }
 
-  # Default theme has no style: nothing to do
-  # Default theme has hillshading: enable hillshading configuration
+  # Built-in themes have no style: nothing to do
+  # Built-in themes have hillshading: enable hillshading configuration
   set theme ${::theme.selection}
-  if {$theme == "(default)"} {
+  if {$theme == "(default)" || \
+      $theme == "(DEFAULT)" || $theme == "(OSMARENDER)"} {
     unset -nocomplain ::style.table ::style.theme
     if {[winfo ismapped .overlays]} {.overlays_show_hide invoke}
     .shading.onmap configure -state normal
@@ -2137,7 +2139,9 @@ proc srv_start {srv} {
     lappend command -m [join $map_list ","]
     if {${::maps.world} == 1} {lappend command -wm}
     set theme [.themes_values get]
-    if {$theme != "(default)"} {
+    if {$theme == "(DEFAULT)" || $theme == "(OSMARENDER)"} {
+      lappend command -t [string trim $theme ()]
+    } elseif {$theme != "(default)"} {
       set theme_file "$::themes_folder/$theme"
       lappend command -t $theme_file
       if {[winfo manager .styles] != ""} {
